a9e3187d82a1dd1e517fc15fcef111beb9620cc3
AnantheshJShet <shetanantheshnlsv@yahoo.in>
2017-09-07 12:24:21 +0530

    Add files via upload

--- CarND-Behavioral-Cloning-P3/drive.py
+++ CarND-Behavioral-Cloning-P3/drive.py
@@ -16,6 +16,8 @@ from keras.models import load_model
 import h5py
 from keras import __version__ as keras_version
 
+import cv2
+
 sio = socketio.Server()
 app = Flask(__name__)
 model = None
@@ -61,7 +63,11 @@ def telemetry(sid, data):
         imgString = data["image"]
         image = Image.open(BytesIO(base64.b64decode(imgString)))
         image_array = np.asarray(image)
-        steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
+        #
+        cropped_image = image_array[60:130, 0:320]
+        resized_image = cv2.resize(cropped_image, (64,64))
+        
+        steering_angle = float(model.predict(resized_image[None, :, :, :], batch_size=1))
 
         throttle = controller.update(float(speed))
 
